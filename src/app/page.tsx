1fa74fc4e65590dd360ed7b4821b36744c425231
JefersonSg <jefersongabri@gmail.com>
2024-05-21 16:07:18 -0300

    removecard da hom e adicionando ao slide, adicionando margint op ao slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import styles from './page.module.css';
 import Voto from '@/components/cards/voto/Voto';
 import SlideCandidatos from '@/components/cards/SlideCandidatos';
-import MediaIdadeCard from '@/components/cards/Idade/MediaIdade';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <SlideCandidatos />
       <Voto />
-      <MediaIdadeCard />
     </main>
   );
 }
